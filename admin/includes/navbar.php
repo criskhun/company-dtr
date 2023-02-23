46eca3efe25f3fc0c38d750267a48d1226937f87
criskhun <crispark30@gmail.com>
2023-02-23 09:18:42 +0800

    Update navbar.php

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -4,7 +4,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>G</b>NET</span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>GNET</b> <p>Group of Companies</p></span>
+      <span class="logo-lg"><b>GNET</b> <h6>Group of Companies</h6></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
