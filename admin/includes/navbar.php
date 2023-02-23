3af74d76c74767b8acf9d183bda8c8b9a31eb723
criskhun <crispark30@gmail.com>
2023-02-23 09:19:22 +0800

    Update navbar.php

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -2,7 +2,7 @@
     <!-- Logo -->
     <a href="#" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>G</b>NET</span>
+      <span class="logo-mini"><b>G</b>NET1</span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>GNET</b> <h6>Group of Companies</h6></span>
     </a>
