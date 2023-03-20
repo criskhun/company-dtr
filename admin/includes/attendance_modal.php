7a50bee43d2df19d70388177cc11c0c6c8363b5c
Saycon <saycon.karl@gmail.com>
2023-03-20 21:08:12 +0800

    Update attendance_modal.php

--- admin/includes/attendance_modal.php
+++ admin/includes/attendance_modal.php
@@ -21,7 +21,7 @@ $dempid= mysqli_query($conn,$qempid);
 
 					  <div class="col-sm-9">
   <select class="form-control" id="empid" name="empid" required>
-    <option value="" selected>- Select Employee Name -</option>
+    <option value="" selected>- Select Employee Namee -</option>
     <?php while($row1 = mysqli_fetch_array($dempid)):;?>
       <option value="<?php echo $row1[0]?>"><?php echo $row1[1] . " " . $row1[2]?></option>
     <?php endwhile; ?>
