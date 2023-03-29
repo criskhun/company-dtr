1c143d4ea184100addbf18fdda7d35068f38479f
criskhun <crispark30@gmail.com>
2023-03-29 22:15:23 +0800

    Update payslip_generate.php

--- admin/payslip_generate.php
+++ admin/payslip_generate.php
@@ -31,7 +31,7 @@
     $pdf->AddPage(); 
     $contents = '';
 
-	$pdf->AddPage();
+	
 
 	$sql = "SELECT *, SUM(num_hr) AS total_hr, SUM(sales.amount) AS totalsales, attendance.employee_id AS empid, employees.employee_id AS employee FROM attendance LEFT JOIN employees ON employees.id=attendance.employee_id LEFT JOIN position ON position.id=employees.position_id LEFT JOIN sales ON sales.employee_id=employees.employee_id WHERE date BETWEEN '$from' AND '$to' AND position.description NOT IN ('UNIT MANAGER','ACCOUNT EXECUTIVE') GROUP BY attendance.employee_id ORDER BY employees.lastname ASC, employees.firstname ASC";
 
@@ -54,6 +54,8 @@
 		$gross = $row['rate'] * $row['total_hr'] + $row['totalsales'];
 		$total_deduction = $deduction + $cashadvance + $salesaprdeduc;
   		$net = $gross - $total_deduction;
+
+		  $pdf->AddPage();
 
 		$contents .= '
 
