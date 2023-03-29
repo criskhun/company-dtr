e3ecb613e90efbfec53634e8e97015f0700cf8d5
criskhun <crispark30@gmail.com>
2023-03-29 20:10:55 +0800

    Update payroll.php

--- admin/payroll.php
+++ admin/payroll.php
@@ -52,7 +52,10 @@
           <div class="box">
             <div class="box-header with-border">
               <div class="pull-left">
-              <button type="button" class="btn btn-success btn-sm btn-flat" id="createpayroll"><span class="glyphicon glyphicon-pencil"></span> Create Payroll</button>
+              <div class="pull-left">
+              <button type="button" class="btn btn-success btn-sm btn-flat" data-toggle="modal" data-target="#myModal">
+                <span  class="glyphicon glyphicon-pencil"></span> Create Payroll</button>
+                </div>
               </div>
               <div class="pull-right">
                 <form method="POST" class="form-inline" id="payForm">
@@ -218,6 +221,64 @@ function getRow(id){
 }
 
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("payroll");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+<div id="myModal" class="modal">
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Modal content goes here.</p>
+  </div>
+</div>
+
+
 </script>
 </body>
 </html>
+<style>
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+</style>
