3fc10a204026b8421fcde63574c58aaa7d14a9ef
Saycon <saycon.karl@gmail.com>
2023-03-30 00:34:20 +0800

    update

--- admin/includes/cashadvance_modal.php
+++ admin/includes/cashadvance_modal.php
@@ -1,3 +1,9 @@
+<?php 
+include 'conn.php'; 
+
+$qempid = "select distinct employee_id, firstname, lastname from employees";
+$dempid= mysqli_query($conn,$qempid);
+?>
 
 <!-- Add -->
 <div class="modal fade" id="addnew">
@@ -11,11 +17,16 @@
           	<div class="modal-body">
             	<form class="form-horizontal" method="POST" action="cashadvance_add.php">
           		  <div class="form-group">
-                  	<label for="employee" class="col-sm-3 control-label">Agent ID</label>
+                  	<label for="employee" class="col-sm-3 control-label">Employee Name</label>
 
                   	<div class="col-sm-9">
-                    	<input type="text" class="form-control" id="employee" name="employee" required>
-                  	</div>
+    <select class="form-control" id="empid" name="empid" required>
+        <option value="" selected disabled>- Select Employee Name NEW -</option>
+        <?php while($row1 = mysqli_fetch_array($dempid)):;?>
+            <option value="<?php echo $row1['employee_id'];?>" ><?php echo $row1['firstname'].' '.$row1['lastname'];?></option>
+        <?php endwhile; ?>
+    </select>
+</div>
                 </div>
                 <div class="form-group">
                     <label for="amount" class="col-sm-3 control-label">Cash Advance</label>
--- admin/includes/overtime_modal.php
+++ admin/includes/overtime_modal.php
@@ -17,7 +17,7 @@ $dempid= mysqli_query($conn,$qempid);
           	<div class="modal-body">
             	<form class="form-horizontal" method="POST" action="attendance_add.php">
           		  <div class="form-group">
-                  	<label for="employee" class="col-sm-3 control-label">Employee ID</label>
+                  	<label for="employee" class="col-sm-3 control-label">Employee Name</label>
 					  <div class="col-sm-9">
     <select class="form-control" id="empid" name="empid" required>
         <option value="" selected disabled>- Select Employee Name NEW -</option>
