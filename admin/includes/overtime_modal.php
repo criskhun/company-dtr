f7c871433e12f1d63b75ad64f288cd859b6d9df5
Saycon <saycon.karl@gmail.com>
2023-03-29 23:57:15 +0800

    Update overtime_modal.php

--- admin/includes/overtime_modal.php
+++ admin/includes/overtime_modal.php
@@ -15,15 +15,16 @@ $dempid= mysqli_query($conn,$qempid);
             	<h4 class="modal-title"><b>Add Overtime</b></h4>
           	</div>
           	<div class="modal-body">
-            	<form class="form-horizontal" method="POST" action="overtime_add.php">
+            	<form class="form-horizontal" method="POST" action="attendance_add.php">
           		  <div class="form-group">
                   	<label for="employee" class="col-sm-3 control-label">Employee ID</label>
-                    <select class="form-control" id="empid" name="empid" required>
-		<option value="" selected>- Select Employee Name -</option>
-		<?php while($row1 = mysqli_fetch_array($dempid)):;?>
-			<option value="<?php echo $row1[0]?>" selected><?php echo $row1[1].' '.$row1[2]?></option>
-		<?php endwhile; ?>
-	</select>
+					  <div class="col-sm-9">
+    <select class="form-control" id="empid" name="empid" required>
+        <option value="" selected disabled>- Select Employee Name NEW -</option>
+        <?php while($row1 = mysqli_fetch_array($dempid)):;?>
+            <option value="<?php echo $row1['employee_id'];?>" ><?php echo $row1['firstname'].' '.$row1['lastname'];?></option>
+        <?php endwhile; ?>
+    </select>
 </div>
                 </div>
                 <div class="form-group">
