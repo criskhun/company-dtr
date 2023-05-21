463fb2d3b3c901767a9efdf57bcda27a3a7cba52
Saycon <saycon.karl@gmail.com>
2023-05-22 05:11:33 +0800

    Update payroll.php

--- admin/payroll.php
+++ admin/payroll.php
@@ -129,7 +129,7 @@
                       $philhealth = 100;
                       $tax = 0.05 * ($gross - 20000);
                       $total_deduction = $deduction + $cashadvance + $salesaprdeduc + $sss + $pagibig + $tax;
-                      $net = $gross - (300 + $cashadvance + $sss + $pagibig + $tax);
+                      $net = $gross - $total_deduction;
 
                       echo "
                         <tr>
