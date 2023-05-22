e2c44b3b381b7080cd253dfd5e899dfb347c9374
Saycon <saycon.karl@gmail.com>
2023-05-22 11:03:56 +0800

    Update holiday.php

--- admin/holiday.php
+++ admin/holiday.php
@@ -21,73 +21,77 @@
     </section>
     <!-- Main content -->
     <section class="content">
-      <?php
-        if(isset($_SESSION['error'])){
-          echo "
-            <div class='alert alert-danger alert-dismissible'>
-              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-              <h4><i class='icon fa fa-warning'></i> Error!</h4>
-              ".$_SESSION['error']."
-            </div>
-          ";
-          unset($_SESSION['error']);
-        }
-        if(isset($_SESSION['success'])){
-          echo "
-            <div class='alert alert-success alert-dismissible'>
-              <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
-              <h4><i class='icon fa fa-check'></i> Success!</h4>
-              ".$_SESSION['success']."
-            </div>
-          ";
-          unset($_SESSION['success']);
-        }
-      ?>
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header with-border">
-              <a href="#addnewhol" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
-            </div>
-            <div class="box-body">
-              <table id="example1" class="table table-bordered">
-                <thead>
-                  <th>Employee Code</th>
-                  <th>Employee Name</th>
-                  <th>Holiday Type</th>
-                  <th>No_Hours</th>
-                  <th>Amount</th>
-                  <th>Date</th>
-                  <th>Tools</th>
-                </thead>
-                <tbody>
-                  <?php
-                    $sql = "SELECT h.*, e.firstname as firstname, e.lastname as lastname FROM employees AS e JOIN holiday AS h ON e.employee_id = h.employee_id; ";
-                    $query = $conn->query($sql);
-                    while($row = $query->fetch_assoc()){
-                      echo "
-                        <tr>
-                        <td>".$row['employee_id']."</td>
-                        <td>".$row['firstname'].' '.$row['lastname']."</td>
-                        <td>".$row['hol_type']."</td>
-                        <td>".number_format($row['hours'], 2)."</td>
-                        <td>".number_format($row['amount'], 2)."</td>
-                        <td>".date('M d, Y', strtotime($row['date']))."</td>
-                        <td>
-                            <button class='btn btn-success btn-sm edit btn-flat' data-id='".$row['id']."'><i class='fa fa-edit'></i> Edit</button>
-                            <button class='btn btn-danger btn-sm delete btn-flat' data-id='".$row['id']."'><i class='fa fa-trash'></i> Delete</button>
-                          </td>
-                        </tr>
-                      ";
-                    }
-                  ?>
-                </tbody>
-              </table>
-            </div>
-          </div>
+  <?php
+    if (isset($_SESSION['error'])) {
+      echo "
+        <div class='alert alert-danger alert-dismissible'>
+          <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+          <h4><i class='icon fa fa-warning'></i> Error!</h4>
+          ".$_SESSION['error']."
+        </div>
+      ";
+      unset($_SESSION['error']);
+    }
+    if (isset($_SESSION['success'])) {
+      echo "
+        <div class='alert alert-success alert-dismissible'>
+          <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+          <h4><i class='icon fa fa-check'></i> Success!</h4>
+          ".$_SESSION['success']."
+        </div>
+      ";
+      unset($_SESSION['success']);
+    }
+  ?>
+
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-header with-border">
+          <a href="#addnewhol" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
+        </div>
+        <div class="box-body">
+          <table id="example1" class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Employee Code</th>
+                <th>Employee Name</th>
+                <th>Holiday Type</th>
+                <th>No_Hours</th>
+                <th>Amount</th>
+                <th>Date</th>
+                <th>Tools</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                $sql = "SELECT h.*, e.firstname as firstname, e.lastname as lastname FROM employees AS e JOIN holiday AS h ON e.employee_id = h.employee_id";
+                $query = $conn->query($sql);
+                while ($row = $query->fetch_assoc()) {
+                  echo "
+                    <tr>
+                      <td>".$row['employee_id']."</td>
+                      <td>".$row['firstname'].' '.$row['lastname']."</td>
+                      <td>".$row['hol_type']."</td>
+                      <td>".number_format($row['hours'], 2)."</td>
+                      <td>".number_format($row['amount'], 2)."</td>
+                      <td>".date('M d, Y', strtotime($row['date']))."</td>
+                      <td>
+                        <button class='btn btn-success btn-sm edit btn-flat' data-id='".$row['id']."'><i class='fa fa-edit'></i> Edit</button>
+                        <button class='btn btn-danger btn-sm delete btn-flat' data-id='".$row['id']."'><i class='fa fa-trash'></i> Delete</button>
+                      </td>
+                    </tr>
+                  ";
+                }
+              ?>
+            </tbody>
+          </table>
         </div>
       </div>
-    </section>   
+    </div>
+  </div>
+</section>
+
   </div>
     
   <?php include 'includes/footer.php'; ?>
