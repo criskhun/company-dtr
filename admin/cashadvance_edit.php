0a67a42bcd0a32bc90be2d754d466911ebdb8906
Saycon <saycon.karl@gmail.com>
2023-06-01 13:57:23 +0800

    Update cashadvance_edit.php

--- admin/cashadvance_edit.php
+++ admin/cashadvance_edit.php
@@ -3,9 +3,12 @@
 
 	if(isset($_POST['edit'])){
 		$id = $_POST['id'];
-		$amount = $_POST['amount'];
+		$amount = $_POST['edit_amount'];
+		$sss = $_POST['edit_sss'];
+		$pagibig = $_POST['edit_pagibig'];
+		$philhealth = $_POST['edit_philhealth'];
 		
-		$sql = "UPDATE cashadvance SET amount = '$amount' WHERE id = '$id'";
+		$sql = "UPDATE cashadvance SET amount = '$amount', sss = '$sss', pagibig = '$pagibig', philhealth = '$philhealth'  WHERE id = '$id'";
 		if($conn->query($sql)){
 			$_SESSION['success'] = 'Mandatory Deductions updated successfully';
 		}
