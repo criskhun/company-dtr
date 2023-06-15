adcac7041af87e4431962ba5cbe5597803b8efc2
Ian Christopher C. Baste <117914828+AW-TA@users.noreply.github.com>
2023-06-16 07:55:37 +0800

    Update employee.php

--- admin/employee.php
+++ admin/employee.php
@@ -47,15 +47,8 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header with-border">
-            <?php
-                if ($_SESSION['user'] == 'admin') {
-                  echo "
-                  <a href='#addnew' data-toggle='modal' class='btn btn-primary btn-sm btn-flat'><i class='fa fa-plus'></i>
-                  New</a>
-                  ";
-                }
-                ?>
-          </div>
+               <a href="#addnew" data-toggle="modal" class="btn btn-primary btn-sm btn-flat"><i class="fa fa-plus"></i> New</a>
+            </div>
             <div class="box-body">
               <table id="example1" class="table table-bordered">
                 <thead>
